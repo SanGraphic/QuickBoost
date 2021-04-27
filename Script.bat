7d8900126d334786530a8af619001b10b230f80a
san <44402346+SanGraphic@users.noreply.github.com>
2021-04-27 13:44:38 +0300

    Update Script.bat

--- Script.bat
+++ Script.bat
@@ -366,22 +366,13 @@ sc start STR
 cls
 @echo off
 color f
-/s /f /q c:\windows\temp\*.*
-rd /s /q c:\windows\temp
-md c:\windows\temp
+rd %temp% /s /q
+md %temp%
+cls
 del /s /f /q C:\WINDOWS\Prefetch
 del /s /f /q %temp%\*.*
 rd /s /q %temp%
 md %temp%
-deltree /y c:\windows\tempor~1
-deltree /y c:\windows\temp
-deltree /y c:\windows\tmp
-deltree /y c:\windows\ff*.tmp
-deltree /y c:\windows\history
-deltree /y c:\windows\cookies
-deltree /y c:\windows\recent
-deltree /y c:\windows\spool\printers
-del c:\WIN386.SWP
 cls 
 
 echo.

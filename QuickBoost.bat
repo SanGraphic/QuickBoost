91d9c0a7c05fae037a0910c49da8a74b586a6486
san <44402346+SanGraphic@users.noreply.github.com>
2021-04-28 03:05:44 +0300

    Add files via upload

--- QuickBoost.bat
+++ QuickBoost.bat
@@ -1,4 +1,6 @@
 @Echo off
+set "params=%*"
+cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
 del C:\Windows\QuickBoostScript.bat
 del C:\Windows\QuickBoostScript.bat
 del C:\Windows\QuickBoostScript.bat

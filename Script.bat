4972681c54346dca46dc75fb1bc6e0e761e84a07
san <44402346+SanGraphic@users.noreply.github.com>
2021-04-29 16:12:08 +0300

    Update Script.bat
    Updated NVProfile

--- Script.bat
+++ Script.bat
@@ -579,7 +579,7 @@ ECHO "%choice%" is not valid, try again
 
 :NV
 powershell -c "Invoke-WebRequest -Uri 'https://cdn.discordapp.com/attachments/460788721789173760/836340936865742918/nvidiaProfileInspector.exe' -OutFile C:\Windows\nvidiaProfileInspector.exe
-powershell -c "Invoke-WebRequest -Uri 'https://cdn.discordapp.com/attachments/460788721789173760/836340587782996038/nvprofile.nip' -OutFile C:\Windows\nvprofile.nip
+powershell -c "Invoke-WebRequest -Uri 'https://cdn.discordapp.com/attachments/460788721789173760/837315087907422228/nvprofile.nip' -OutFile C:\Windows\nvprofile.nip
 start "" /wait "C:\Windows\nvidiaProfileInspector.exe" "C:\Windows\nvprofile.nip"
 timeout 2 >nul
 goto :end

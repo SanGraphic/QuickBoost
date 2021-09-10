a7a5b05633dc8497a1ddce152d582e6ca635a490
san <44402346+SanGraphic@users.noreply.github.com>
2021-09-10 20:51:05 +0300

    Add files via upload

---
+++ v2/Devices crashing in settings fix/Bluetooth Fix.bat
@@ -0,0 +1,2 @@
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\DevicesFlowUserSvc" /v Start /t REG_DWORD /d "3" /f
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\DevicePickerUserSvc" /v Start /t REG_DWORD /d "3" /f

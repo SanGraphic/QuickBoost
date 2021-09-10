92a016694414c76bae324a803213c7f9d3724df1
san <44402346+SanGraphic@users.noreply.github.com>
2021-09-10 16:14:57 +0300

    Add files via upload

---
+++ v2/Advanced/HardwareDataQueueSize.bat
@@ -0,0 +1,4 @@
+@echo off
+Reg.exe add "HKLM\SYSTEM\CurrentControlSet\Services\mouclass\Parameters" /v "MouseDataQueueSize" /t REG_DWORD /d "20" /f
+Reg.exe add "HKLM\SYSTEM\CurrentControlSet\Services\kbdclass\Parameters" /v "KeyboardDataQueueSize" /t REG_DWORD /d "20" /f
+exit

6d6efcea0d243fdc91303d46217d146b937f6143
san <44402346+SanGraphic@users.noreply.github.com>
2021-09-10 20:52:06 +0300

    Update Task Manager not responding when closing it.cmd

--- v2/Task Manager not responding when closing it.cmd
+++ v2/Task Manager not responding when closing it.cmd
@@ -1,2 +1,4 @@
+@echo off
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\WMI\Autologger\DiagLog" /v "Start" /t REG_DWORD /d "1" /f 
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\WMI\Autologger\WdiContextLog" /v "Start" /t REG_DWORD /d "1" /f 
+exit

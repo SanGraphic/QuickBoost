b84cda555dd2f92d005b1c1362aba68a8a0ab767
san <44402346+SanGraphic@users.noreply.github.com>
2021-09-10 20:52:26 +0300

    Update Bluetooth Fix.bat

--- v2/Devices crashing in settings fix/Bluetooth Fix.bat
+++ v2/Devices crashing in settings fix/Bluetooth Fix.bat
@@ -1,2 +1,4 @@
+@echo off
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\DevicesFlowUserSvc" /v Start /t REG_DWORD /d "3" /f
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\DevicePickerUserSvc" /v Start /t REG_DWORD /d "3" /f
+exit

6d03a060bededa4933ea3f7ad0f4f0042fe159f8
san <sanayad24@gmail.com>
2022-07-12 12:15:00 +0300

    12

---
+++ QuickBoost/Views/Pages/Debloat.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System;
+using System.Net;
+using System.Diagnostics;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QuickBoost.Views.Pages
+{
+    /// <summary>
+    /// Interaction logic for Debloat.xaml
+    /// </summary>
+    public partial class Debloat : Page
+    {
+        public Debloat()
+        {
+            InitializeComponent();
+        }
+
+        private void OldContext(object sender, RoutedEventArgs e)
+        {
+            Registry.SetValue("HKEY_CURRENT_USER\\Software\\Classes\\CLSID\\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\\InprocServer32", "", "");
+            string message = "Windows 10 Style Context Menu Applied!";
+            MessageBox.Show(message);
+            Process p = new Process();
+            foreach (Process exe in Process.GetProcesses())
+            {
+                if (exe.ProcessName == "explorer")
+                    exe.Kill();
+            }
+            Process.Start("explorer.exe");
+        }
+
+        private void NewContext(object sender, RoutedEventArgs e)
+        {
+            ProcessStartInfo contextdel = new ProcessStartInfo("WT.exe");
+            contextdel.UseShellExecute = true;
+            contextdel.Arguments = "REG DELETE \"HKCU\\Software\\Classes\\CLSID\\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\" /f";
+            Process.Start(contextdel);
+            string message = "Windows 11 Style Context Menu Applied!";
+            MessageBox.Show(message);
+        }
+
+        private void HiddenFolders(object sender, RoutedEventArgs e)
+        {
+            Registry.SetValue("HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced", "Hidden", "00000001", RegistryValueKind.DWord);
+            string message = "Hidden Folders Visible now!";
+            MessageBox.Show(message);
+        }
+
+        private void HideHiddenFolders(object sender, RoutedEventArgs e)
+        {
+            Registry.SetValue("HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced", "Hidden", "00000000", RegistryValueKind.DWord);
+            string message = "Hidden Folders Invisible now!";
+            MessageBox.Show(message);
+        }
+
+        private void Extensions(object sender, RoutedEventArgs e)
+        {
+            Registry.SetValue("HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced", "HideFileExt", "00000000", RegistryValueKind.DWord);
+            string message = "File Extensions visible now!";
+            MessageBox.Show(message);
+        }
+
+        private void HideExtensions(object sender, RoutedEventArgs e)
+        {
+            Registry.SetValue("HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced", "HideFileExt", "00000001", RegistryValueKind.DWord);
+            string message = "File Extensions invisible now!";
+            MessageBox.Show(message);
+        }
+    }
+}

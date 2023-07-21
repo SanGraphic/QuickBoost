ecae8dcaedf7e9c1656f7ebfa84cd89b44625478
san <44402346+SanGraphic@users.noreply.github.com>
2023-07-21 11:18:15 +0300

    Fix PowerPlan (Temporary solution)

--- v2/ImportPowerPlan.bat
+++ v2/ImportPowerPlan.bat
@@ -1,8 +1,8 @@
 @echo off
 Echo Installing QuickBoost PowerPlan
 echo.
-powershell -c "Invoke-WebRequest -Uri 'https://cdn.discordapp.com/attachments/460788721789173760/836157669298667570/SanGraphicPOWERPLAN.pow' -OutFile C:\Windows\SanGraphicPOWERPLAN.pow
-powercfg -import "C:\Windows\SanGraphicPOWERPLAN.pow" f42fe57c-e762-287e-984a-4e9613d9e9d3
+powershell -c "Invoke-WebRequest -Uri 'https://github.com/SanGraphic/QuickBoost/raw/main/v2/Adamx's%20Power%20Plan.pow' -OutFile C:\Windows\SanGraphicPOWERPLAN.pow
+powercfg -import "C:\Windows\SanGraphicPOWERPLAN.pow" 945fe57c-e762-487e-984a-4e9213d9e9d7
 del /f "C:\Windows\SanGraphicPOWERPLAN.pow"
-powercfg -SETACTIVE "f42fe57c-e762-287e-984a-4e9613d9e9d3"
+powercfg -SETACTIVE "945fe57c-e762-487e-984a-4e9213d9e9d7"
 exit

62c2a92b47d11f127693b44f6585c57e4c7a1cb9
san <44402346+SanGraphic@users.noreply.github.com>
2023-07-21 11:10:37 +0300

    Update TimerResolutionService.bat

--- v2/TimerResolutionService.bat
+++ v2/TimerResolutionService.bat
@@ -1,7 +1,7 @@
 @echo off
 Echo Installing Timer Resolution Service
 echo.
-powershell Invoke-WebRequest "https://cdn.discordapp.com/attachments/460788721789173760/836161440103923732/SetTimerResolutionService.exe" -OutFile "C:\Windows\SetTimerResolutionService.exe"
+powershell Invoke-WebRequest "https://github.com/SanGraphic/QuickBoost/raw/main/v2/SetTimerResolutionService%20(1).exe" -OutFile "C:\Windows\SetTimerResolutionService.exe"
 "C:\Windows\SetTimerResolutionService.exe" -install
 sc start STR
 exit
